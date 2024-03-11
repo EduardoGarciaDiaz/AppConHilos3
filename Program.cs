5ce0a0982a399adbd99af39adbfbcfb599bcfd40
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-03-11 17:14:48 -0600

    Subprocesos máximos y mínimos del thread pool

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿namespace AppConHilos3;
 
-class Program
-{
-}
-
 class Persona
 {
     public string Nombre { get; set; }
@@ -17,6 +13,63 @@ class Persona
         this.Sexo = sexo;
     }
 
+
+    static void Main(string [] args)
+    {
+        // Variables para hilos máximos, mínimos y puertos (INput/output completion port, IOCP)
+        int workers, ports;
+
+        // Obtener el maximo numero de hilos
+        ThreadPool.GetMaxThreads(out workers, out ports);
+        Console.WriteLine($"Máximos hilos de trabajo: {workers} ");
+        Console.WriteLine($"Máximos puertos para hilos: {ports}");
+
+        // Obtener un numero mínimo de hilos
+        ThreadPool.GetMinThreads(out workers, out ports);
+        Console.WriteLine($"Minimos hilos de trabajo: {workers} ");
+        Console.WriteLine($"Minimos puertos (completion port) para hilos: {ports}");
+
+        // Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles: {workers}");
+        Console.WriteLine($"Hilos de puerto (completion port) disponibles: {ports}");
+
+        // Obtenga el número total de procesadores disponibles en la máquina
+        int processCount = Environment.ProcessorCount;
+        Console.WriteLine($"No. de procesadores disponibles en el sistema:: {processCount}");
+        Console.WriteLine($"---------------------------------");
+
+        //Utilice ThreadPool para un hilo de trabajo
+        ThreadPool.QueueUserWorkItem(TareaDeFondo);
+        //Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles despues del hilo 1: {workers} ");
+
+        //Crea un objeto y lo envía al hilo de trabajo de ThreadPool 
+        Persona p = new Persona("Eduardo García", 21, "Hombre");
+        ThreadPool.QueueUserWorkItem(TareaDeFondoConParametro, p);
+        // Obtener hilos disponibles 
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles después del hilo 2: {workers} ");
+
+
+        // Esperamos a que ambos hilos finalicen 
+        Thread.Sleep(2000); 
+        // Obtener hilos disponibles 
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles al final: {workers} ");
+    
+        Console.ReadKey();
+    }
+
+    // Tarea de fondo 1
+    static void TareaDeFondo(Object? stateInfo)
+    {
+        Console.WriteLine($"Hilo 1: Hola soy un hilo sin uso parámetros desde ThreadPool.");
+        Thread.Sleep(1500);
+    }
+
+    // Tarea de Fondo 2
     static void TareaDeFondoConParametro(Object? stateInfo)
     {
         if (stateInfo == null) 
@@ -24,23 +77,9 @@ class Persona
             return;
         }
 
-        //Obtenemos el hilo actual
-        Thread currentThread = Thread.CurrentThread;
-        // Imprimimos el tipo
-        Console.WriteLine("Es un thread background: {0}", currentThread.IsBackground);
-
         Persona data = (Persona)stateInfo;
-        Console.WriteLine($"Hola {data.Nombre} desde ThreadPool.");
-        Thread.Sleep(1000);
+        Console.WriteLine($"Hilo 2: Hola {data.Nombre}, tu eedad es {data.Edad}.");
+        Thread.Sleep(500);
     }
 
-    static void Main(string [] args)
-    {
-        // Crea un objeto y lo envía el hilo de trabajo de ThreadPool
-        Persona p = new Persona("Eduardo García", 21, "Hombre");
-        ThreadPool.QueueUserWorkItem(TareaDeFondoConParametro, p);
-
-        //Esta instrucción es necesaria para que no termine el programa
-        Console.ReadKey();
-    }
 }
